79b50c9a46c4fe61b3be2d5e78af5085cb602e88
developerHub01 <shakil102043@gmail.com>
2026-05-04 22:28:30 +0600

    prettier installed

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter, Poppins } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+
+export const lusitana = Poppins({
+  weight: ["600", "700", "800", "900"],
+  subsets: ["latin"],
+});
