d37603c70597ff511f86a81fa4d1c7d58551748f
developerHub01 <shakil102043@gmail.com>
2026-05-04 22:29:23 +0600

    basic routing added

---
+++ app/(app)/(marketing)/layout.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const MarketingLayout = ({ children }: Props) => {
+  return <div>{children}</div>;
+};
+
+export default MarketingLayout;
