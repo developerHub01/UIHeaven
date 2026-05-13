4f8d085fd910ae296ea01374a7ffa3ab6165ec69
developerHub01 <shakil102043@gmail.com>
2026-05-13 23:40:23 +0600

    google and github OAuth added for local supabase

--- app/(app)/auth/page.tsx
+++ app/(app)/auth/page.tsx
@@ -1,7 +1,37 @@
-import React from "react";
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { createClient } from "@/lib/supabase/client";
 
 const AuthPage = () => {
-  return <div></div>;
+  const supabase = createClient();
+
+  const handleGoogleLogin = async () => {
+    const result = await supabase.auth.signInWithOAuth({
+      provider: "google",
+      options: {
+        redirectTo: `http://localhost:3000/auth/callback`,
+      },
+    });
+
+    console.log(result);
+  };
+
+  const handleGithubLogin = async () => {
+    await supabase.auth.signInWithOAuth({
+      provider: "github",
+      options: {
+        redirectTo: `${window.location.origin}/auth/callback`,
+      },
+    });
+  };
+
+  return (
+    <div>
+      <Button onClick={handleGoogleLogin}>Signup with Google</Button>
+      <Button onClick={handleGithubLogin}>Signup with Github</Button>
+    </div>
+  );
 };
 
 export default AuthPage;
